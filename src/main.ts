7d237bd708fe1044faaef1f2c2744475d3eb47a6
Norayr <norayr.babayann@gmail.com>
2026-07-11 14:34:16 +0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(helmet())
   
   app.enableCors({
-    origin:"http://localhost:5173",
+    origin:["http://localhost:5173","https://software-app-frontend.onrender.com"],
     credentials: true
   })
 
